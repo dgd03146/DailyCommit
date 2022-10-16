46c2db50f74cbb16bd5e3e34db9b3dbdc185a5b7
jeong <ibory1220@naver.com>
2022-10-16 22:49:23 +0900

    import namespace when it needs

--- typeScript/understanding-typeScript/prj-drag-and-drop/src/app.ts
+++ typeScript/understanding-typeScript/prj-drag-and-drop/src/app.ts
@@ -1,10 +1,7 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
 /// <reference path="components/project-input.ts" />
 /// <reference path="components/project-list.ts" />
+
+// 해당 파일이 필요한 것들을 필요할 때 이용할 수 있도록 해야한다.
 
 namespace App {
   new ProjectInput();
