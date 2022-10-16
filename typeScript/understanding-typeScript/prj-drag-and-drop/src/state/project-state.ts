49ab65112408e529d978e0fcf07d7fc65cb89a0b
jeong <ibory1220@naver.com>
2022-10-17 00:07:58 +0900

    namespace & ES module

--- typeScript/understanding-typeScript/prj-drag-and-drop/src/state/project-state.ts
+++ typeScript/understanding-typeScript/prj-drag-and-drop/src/state/project-state.ts
@@ -55,3 +55,10 @@ export class ProjectState extends State<Project> {
 }
 
 export const projectState = ProjectState.getInstance();
+// 여러개의 파일에서 import 되고 있는데 총 몇번 실행이 될까???
+// 다른 어떤 파일에 의해 파일이 처음으로 import 되었을때 1회 실행됨
+// 다른 파일이 그 같은 파일을 다시 임포트하는 경우 다시 실행되지 않는다.
+
+// ES 모듈과 namespace 중 ES 모듈 방법을 권장한다. 타입 안전을 확보할 수 있고, 모든 파일이 무엇을 필요로 하는지 분명히 명시하기 때문
+// namespace는 임포트 했던 하나의 파일이 삭제되면, 다른 파일이 알림 없이 중단되는 점입니다. 적어도 코드 실행 이 전에는 알림 없습니다.
+// 네임스페이스는 비교적 작은 프로젝트, 그 외의 경우에는, ES 모듈을 사용!!!
