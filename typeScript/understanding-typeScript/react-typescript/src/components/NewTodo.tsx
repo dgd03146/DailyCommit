475dda15a3c61d34fc0306ceaa61076d9b5a2800
jeong <ibory1220@naver.com>
2022-10-18 16:59:49 +0900

    cross-component communication

--- typeScript/understanding-typeScript/react-typescript/src/components/NewTodo.tsx
+++ typeScript/understanding-typeScript/react-typescript/src/components/NewTodo.tsx
@@ -1,13 +1,18 @@
 import React, { useRef } from 'react';
 
-const NewTodo = () => {
+// 평상시에 인터페이스를 만들겠지만 type을 사용하는것도 문제가 없다.
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo = ({ onAddTodo }: NewTodoProps) => {
   const textInputRef = useRef<HTMLInputElement>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
     const enteredText = textInputRef.current!.value; // 타입스크립트에게 이값이 설정될거라고 알려줌
-    console.log(enteredText);
+    onAddTodo(enteredText);
   };
 
   return (
--- typeScript/understanding-typeScript/react-typescript/src/components/TodoList.tsx
+++ typeScript/understanding-typeScript/react-typescript/src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-interface TodoListProps {
+export interface TodoListProps {
   items: { id: string; text: string }[];
 }
 
