26c63b37564c4af5699363ed6cfc05522c728335
jeong <ibory1220@naver.com>
2022-10-18 10:28:18 +0900

    get geolocation with axios

---
+++ typeScript/understanding-typeScript/prj-libs-03-fetching-coordinates/src/app.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyDquRb9TM0YoF4g1PAl6sdFjKrtEZe-6V0';
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  // 타입스크립트에게 우리가 기대하는 응답을 전달할 수 있다.
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Could not fetch location!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener('submit', searchAddressHandler);
