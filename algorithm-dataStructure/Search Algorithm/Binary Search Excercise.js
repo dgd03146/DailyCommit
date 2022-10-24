99f233c6b7cb42ac4ef1f9801f8515ea8e867c2b
jeong <ibory1220@naver.com>
2022-10-24 19:04:13 +0900

    bubble & insertion & selection sort algorithm

--- algorithm-dataStructure/Search Algorithm/Binary Search Excercise.js
+++ algorithm-dataStructure/Search Algorithm/Binary Search Excercise.js
@@ -11,6 +11,7 @@ function binarySearch(array, value) {
     }
 
     mid = Math.floor((start + end) / 2);
+    console.log('start:', start, 'end:', end, 'mid', mid);
   }
 
   if (array[mid] === value) {
@@ -20,34 +21,35 @@ function binarySearch(array, value) {
   return -1;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5], 2));
-console.log(binarySearch([1, 2, 3, 4, 5], 3));
-console.log(binarySearch([1, 2, 3, 4, 5], 5));
+// console.log(binarySearch([1, 2, 3, 4, 5], 2));
+// console.log(binarySearch([1, 2, 3, 4, 5], 3));
+// console.log(binarySearch([1, 2, 3, 4, 5], 5));
 console.log(binarySearch([1, 2, 3, 4, 5], 6));
-console.log(
-  binarySearch(
-    [
-      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
-      99
-    ],
-    10
-  )
-);
-console.log(
-  binarySearch(
-    [
-      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
-      99
-    ],
-    95
-  )
-);
-console.log(
-  binarySearch(
-    [
-      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
-      99
-    ],
-    100
-  )
-);
+
+// console.log(
+//   binarySearch(
+//     [
+//       5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+//       99
+//     ],
+//     10
+//   )
+// );
+// console.log(
+//   binarySearch(
+//     [
+//       5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+//       99
+//     ],
+//     95
+//   )
+// );
+// console.log(
+//   binarySearch(
+//     [
+//       5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+//       99
+//     ],
+//     100
+//   )
+// );
---
+++ algorithm-dataStructure/Sort/Selection sort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    //최솟값이 처음에 시작한 값이 아닐 경우 바꾼다.
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+selectionSort([34, 22, 10, 19, 17]);
