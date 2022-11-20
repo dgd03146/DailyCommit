a7d846724b2a4ad23baecf3018824309a7f7ca56
jeong <ibory1220@naver.com>
2022-11-20 20:33:43 +0900

    daily algorithm

--- algorithm-dataStructure/Problem Solving/Sliding Window/minSubArrayLen.js
+++ algorithm-dataStructure/Problem Solving/Sliding Window/minSubArrayLen.js
@@ -7,11 +7,13 @@ function minSubArrayLen(nums, sum) {
   while (start < nums.length) {
     // if current window doesn't add up to the given sum then
     // move the window to right
-
     if (total < sum && end < nums.length) {
       total += nums[end];
       end++;
-    } else if (total >= sum) {
+    }
+    // if current window adds up to at least the sum given then
+    // we can shrink the window
+    else if (total >= sum) {
       minLen = Math.min(minLen, end - start);
       total -= nums[start];
       start++;
@@ -21,14 +23,7 @@ function minSubArrayLen(nums, sum) {
       break;
     }
   }
-
-  return minLen === Infinity ? 0 : minLen;
+  // return minLen === Infinity ? 0 : minLen;
 }
 
 console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7));
-// console.log(minSubArrayLen([2, 1, 6, 5, 4], 9));
-// console.log(minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52));
-// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39));
-// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55));
-// console.log(minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11));
-// console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 95));
