23f7698c4c06bf24331df22b25176f7b46dddfda
jeong <ibory1220@naver.com>
2023-04-19 19:54:18 +0100

    Link

--- next/Next.js Dream Coding/note/src/app/products/layout.tsx
+++ next/Next.js Dream Coding/note/src/app/products/layout.tsx
@@ -1,15 +1,16 @@
+import Link from 'next/link';
 import styles from './layout.module.css';
 
 export default function ProductsLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
     <>
       <nav className={styles.nav}>
-        <a href=''>여성옷</a>
-        <a href=''>남성옷</a>
+        <Link href="/products/woman">여성옷</Link>
+        <Link href="/products/man">남성옷</Link>
       </nav>
       <section className={styles.product}>{children}</section>
     </>
--- next/Next.js Dream Coding/note/src/app/products/page.tsx
+++ next/Next.js Dream Coding/note/src/app/products/page.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
 
+const products = ['pants', 'skirts', 'shoes', 'skirt'];
+
 const ProductsPage = () => {
-  return <h1>제품 소개 페이지!</h1>;
+  return (
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ProductsPage;
