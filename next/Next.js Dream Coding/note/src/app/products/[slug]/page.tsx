3b40f965c89fd3d0d531e9652484787caeeb2a5a
jeong <ibory1220@naver.com>
2023-04-20 13:19:33 +0100

    SSG rendering

--- next/Next.js Dream Coding/note/src/app/products/[slug]/page.tsx
+++ next/Next.js Dream Coding/note/src/app/products/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { notFound } from 'next/navigation';
 import { getProduct, getProducts } from '@/service/products';
+import { Metadata } from 'next';
 
 type Props = {
   params: {
@@ -8,30 +9,38 @@ type Props = {
   };
 };
 
-export function generateMetadata({ params }: Props) {
-  return {
-    title: `제품의 이름: ${params.slug}`
-  };
-}
+export const generateMetadata = async ({
+  params
+}: Props): Promise<Metadata> => {
+  const { slug } = params;
 
-const PantsPage = ({ params: { slug } }: Props) => {
-  const product = getProduct(slug);
+  const product = await getProduct(slug);
+
+  return {
+    title: `Wonderful Items | ${product?.name.toUpperCase()}`,
+    description: `Check our wonderful ${product?.name}!`,
+    metadataBase: null
+  };
+};
+
+const PantsPage = async ({ params: { slug } }: Props) => {
+  const product = await getProduct(slug);
 
   if (!product) {
     notFound();
   }
 
   // 서버 파일에 있는 데이터중 해당 제품의 정보를 찾아서 그걸 보여줌
-  return <h1>{product} 제품 설명 페이지</h1>;
+  return <h1>{product.name} 제품 설명 페이지</h1>;
 };
 
 export default PantsPage;
 
 export async function generateStaticParams() {
   // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
-  const products = getProducts();
+  const products = await getProducts();
 
   return products.map((product) => ({
-    slug: product
+    slug: product.id
   }));
 }
--- next/Next.js Dream Coding/note/src/app/products/page.tsx
+++ next/Next.js Dream Coding/note/src/app/products/page.tsx
@@ -2,17 +2,17 @@ import { getProducts } from '@/service/products';
 import Link from 'next/link';
 import React from 'react';
 
-const ProductsPage = () => {
+const ProductsPage = async () => {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서 그걸 보여줌
-  const products = getProducts();
+  const products = await getProducts();
 
   return (
     <>
       <h1>제품 소개 페이지!</h1>
       <ul>
-        {products.map((product, index) => (
+        {products.map(({ id, name }, index) => (
           <li key={index}>
-            <Link href={`products/${product}`}>{product}</Link>
+            <Link href={`products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
@@ -21,3 +21,6 @@ const ProductsPage = () => {
 };
 
 export default ProductsPage;
+
+// 서버 컴포넌트는 async 함수로 정의 가능(즉, 서버 컴포넌트를 async 함수로 정의 하는 것은 전혀 이상하지 않음)
+// 다만, 서버 컴포넌트 "자체"가 아직은 실험적 기능으로 자주 보이는 패턴이 아님
