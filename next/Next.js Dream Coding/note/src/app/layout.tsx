2ce6bf1e4f86950682d72b738efd19a3c918a1f3
jeong <ibory1220@naver.com>
2023-04-21 16:19:54 +0100

    Font

--- next/Next.js Dream Coding/note/src/app/layout.tsx
+++ next/Next.js Dream Coding/note/src/app/layout.tsx
@@ -2,6 +2,11 @@ import Link from 'next/link';
 import './globals.css';
 import styles from './layout.module.css';
 import { Metadata } from 'next';
+import { Open_Sans } from '@next/font/google';
+import { Nanum_Gothic } from '@next/font/google';
+
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({ weight: '700', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '멋진 제품 사이트',
@@ -18,10 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={sans.className}>
       <body>
         <header className={styles.header}>
-          <h1>Demo Note App</h1>
+          <h1 className={gothic.className}>Demo Note App</h1>
           <nav className={styles.nav}>
             <Link href="/products">Products</Link>
             <Link href="/contact">Contact</Link>
